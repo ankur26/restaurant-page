e7d03fd1158493c5c80ffc881751a9a6072c67ea
Ankur Bhatkalkar <ab7869@nyu.edu>
2021-12-12 16:02:27 -0500

    Added reviews to contact page to make it look fuller

--- src/contactpage.js
+++ src/contactpage.js
@@ -81,11 +81,42 @@ const renderForm = () => {
     return form;
 }
 
+const renderReviewDiv = (review,customer) => {
+    let div = helper.renderDiv();
+    let reviewh3 = helper.renderElement({
+        element:"h3",
+    });
+    reviewh3.textContent = review;
+    let customerName = helper.renderElement({
+        element:"p",
+    });
+    customerName.textContent = customer;
+    div.appendChild(reviewh3);
+    div.appendChild(customerName);
+    return div;
+}
+
 const render = () =>{
     let maincontent = helper.renderDiv();
+    let reviewDiv = helper.renderDiv();
+    let review1 = renderReviewDiv("Absolutely Fantastic Food!","Random Customer 1");
+    let review2 = renderReviewDiv("The Idlis are to die for !","Random Customer 2");
+    let review3 = renderReviewDiv("Fantastic Ambience and food to match","Random Customer 3");
+    reviewDiv.appendChild(review1);
+    reviewDiv.appendChild(review2);
+    reviewDiv.appendChild(review3);
+    let contactFormDiv = helper.renderDiv();
+    let contactFormTitle = helper.renderElement({
+        element:"h2",
+    });
+    contactFormTitle.textContent = "Contact Us!";
     let form = renderForm();
-    maincontent.appendChild(form);
-    return form;
+    contactFormDiv.appendChild(contactFormTitle);
+    contactFormDiv.appendChild(form);
+    maincontent.appendChild(reviewDiv);
+    maincontent.appendChild(contactFormDiv);
+    // maincontent.appendChild(form);
+    return maincontent;
 }
 
 export {render};
